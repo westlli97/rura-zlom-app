49413f3a8540f04989c292ef2c40807797b26c1d
Westlli <marek.lach97@gmail.com>
2025-04-12 17:37:18 +0200

    Dodanie materialow oraz date dodania

--- containers/models.py
+++ containers/models.py
@@ -5,19 +5,23 @@ class Container(models.Model):
     MATERIAL_CHOICES = [
         ('AL', 'Aluminium'),
         ('A304', 'AISI 304'),
+        ('A304_Poler', 'AISI 304 Poler'),
         ('A316', 'AISI 316'),
+        ('A316_Poler', 'AISI 316 Poler'),
         ('MS', 'Mosiądz'),
     ]
     SHAPE_CHOICES = [
         ('ROUND', 'Okrągły'),
         ('SQUARE', 'Kwadratowy'),
         ('RECT', 'Prostokątny'),
+        ('Marcepan',)
     ]
 
     material = models.CharField(max_length=10, choices=MATERIAL_CHOICES)
     shape = models.CharField(max_length=10, choices=SHAPE_CHOICES)
     weight_kg = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     kg_per_metr = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.get_material_display()} - {self.get_shape_display()}"
