03c7f68d752863aa90937a381d40f0bfd85f94c6
Westlli <marek.lach97@gmail.com>
2025-04-12 17:27:37 +0200

    Dodano model Container

--- containers/models.py
+++ containers/models.py
@@ -1,3 +1,23 @@
+
 from django.db import models
 
-# Create your models here.
+class Container(models.Model):
+    MATERIAL_CHOICES = [
+        ('AL', 'Aluminium'),
+        ('A304', 'AISI 304'),
+        ('A316', 'AISI 316'),
+        ('MS', 'Mosiądz'),
+    ]
+    SHAPE_CHOICES = [
+        ('ROUND', 'Okrągły'),
+        ('SQUARE', 'Kwadratowy'),
+        ('RECT', 'Prostokątny'),
+    ]
+
+    material = models.CharField(max_length=10, choices=MATERIAL_CHOICES)
+    shape = models.CharField(max_length=10, choices=SHAPE_CHOICES)
+    weight_kg = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    kg_per_metr = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"{self.get_material_display()} - {self.get_shape_display()}"
