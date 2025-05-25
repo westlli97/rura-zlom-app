e973ce0addba14582f55559512b07dee4a498cbf
Westlli <marek.lach97@gmail.com>
2025-05-25 19:59:09 +0200

    build add

---
+++ frontend/build/precache-manifest.50ffbd6c917042b77c62e382ab7d1568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c86650269451de9456917c7767e873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069b88a932309fc88254",
+    "url": "/static/css/main.778cf339.chunk.css"
+  },
+  {
+    "revision": "c79903df274c9e218638",
+    "url": "/static/js/2.2213b3fc.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "069b88a932309fc88254",
+    "url": "/static/js/main.6f6f78a9.chunk.js"
+  },
+  {
+    "revision": "2bd38feb6fb022a94fd0",
+    "url": "/static/js/runtime~main.cd66bee5.js"
+  }
+]);
