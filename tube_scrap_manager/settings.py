456fb16442e14c7c11b6ad5901a2e41fa1aa5543
Westlli <marek.lach97@gmail.com>
2025-05-25 20:28:13 +0200

    fix:test

--- tube_scrap_manager/settings.py
+++ tube_scrap_manager/settings.py
@@ -30,14 +30,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-kg@k0hd1#zpvdf#nwl5(%ewjdb_^tdalc$4m!@4$v_azdjk6t4'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,6 +50,8 @@ MIDDLEWARE = [
 ]
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'zlom-app.onrender.com']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
